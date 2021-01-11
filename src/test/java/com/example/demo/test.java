3bbd485a0685f76893ebb3cc97e34d11a86bd71d
MYH <1973478958@qq.com>
2021-01-11 15:03:13 +0800

    idea commit

--- src/test/java/com/example/demo/test.java
+++ src/test/java/com/example/demo/test.java
@@ -6,21 +6,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class test {
-@Test
-    public  void test1(){
-        int start=6000;
-        int end=7000;
+    @Test
+    public void test1() {
+        int start = 6000;
+        int end = 7000;
 
 
         System.out.println("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
         System.out.println("<urlset>");
-        for (int i=start;i<=end;i++){
+        for (int i = start; i <= end; i++) {
             Date date = new Date();
-            SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String time=simpleDateFormat.format(date);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String time = simpleDateFormat.format(date);
             System.out.println("<url>");
-            System.out.println("<loc>http://www.aoklite.com/info/"+i+".html</loc>");
-            System.out.println("<lastmod>"+time+"</lastmod>");
+            System.out.println("<loc>http://www.aoklite.com/info/" + i + ".html</loc>");
+            System.out.println("<lastmod>" + time + "</lastmod>");
             System.out.println("<changefreq>weekly</changefreq>");
             System.out.println("<priority>0.8</priority>");
             System.out.println("</url>");
@@ -29,8 +29,17 @@ public class test {
 
 
         System.out.println("\n\n---------------------------\n\n");
-        for (int i =start;i<=end;i++) {
-            System.out.println("http://www.aoklite.com/info/"+i+".html");
+        for (int i = start; i <= end; i++) {
+            System.out.println("http://www.aoklite.com/info/" + i + ".html");
+        }
+    }
+
+    @Test
+    public void test2() {
+        for (int i = 1; i <=200; i++) {
+
+
+            System.out.println("\t\t<img src=\"https://aoklite.oss-cn-beijing.aliyuncs.com/movie/"+i+".png\">");
         }
     }
 }
